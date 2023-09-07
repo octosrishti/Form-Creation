5b1e2353a2fb35da81584327f3567862c215711d
octosrishti <srishti.21ug3023@iiitranchi.ac.in>
2023-09-07 17:36:39 +0530

    dash

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Student from './components/Student';
+import './App.css';
+export default function App() {
+  return (
+    <div className='Form'>
+      <Student/>
+      
+    </div>
+  )
+}
